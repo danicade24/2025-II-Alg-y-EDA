59a6b6c99dd010d266f1e4f09211b1e7daad80ce
Daniela <daniela.cadena.v@uni.pe>
2025-10-14 19:13:56 -0500

    TODO:Se agrego balanceo en AVL

---
+++ Makefile
@@ -0,0 +1,24 @@
+CXX = g++
+CXXFLAGS = -std=c++17 -Wall -g -pthread # Añadido -pthread
+LDFLAGS = -pthread # Añadido -pthread
+
+TARGET = main
+SRCS = main.cpp \
+       hilos.cpp \
+	   DemoVector.cpp \
+	   DemoList.cpp
+
+OBJS = $(SRCS:.cpp=.o)
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	$(CXX) $(LDFLAGS) $^ -o $@
+
+%.o: %.cpp
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJS) $(TARGET)
+
+.PHONY: all clean
---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "vector.h"
+#include "DemoVector.h"
+#include "hilos.h"
+#include "linkedlist.h"
+#include "doublelinkedlist.h"
+
+using namespace std;
+
+/* Revisado por:
+   1. Ernesto Cuadros-Vargas
+   4. Héctor Bobbio Hermoza 
+   2. Jharvy Jonas Cadillo Tarazona
+   20. Ortiz Lozano Eric Hernan
+   22. Chandler Steven Perez Cueva
+*/
+
+// Forma 1 de Compilar: 
+// g++ -std=c++17 -Wall -g -pthread -o main main.cpp
+// Forma #2 de Compilar (requiere el archivo Makefile)
+// make
+
+int main(){
+    cout << "Hello Alg y EDA-UNI" << endl;
+    // DemoThreads();
+    [[maybe_unused]] int x = 5;
+    // DemoVector();
+    DemoLinkedList();
+    DemoDoubleLinkedList();
+    return 0;
+}
+
+// int main(int nArgs, char *pArgs[]){
+//     cout << "Hello Alg y EDA-UNI (forma #2)" << endl;
+//     int i;
+//     for(i = 0 ; i < nArgs ; ++i){
+//         cout << pArgs[i] << endl;
+//     }
+// }
+
+
+
